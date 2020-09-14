120f1d852cf464a5d2dbc54d63e9b1aced5c0052
cdaza <cdaza933@hotmail.com>
2020-09-13 21:38:11 -0500

    add logic to run test

--- backend/tests/tasks.py
+++ backend/tests/tasks.py
@@ -7,10 +7,12 @@ import subprocess
 
 
 @app.task()
-def run_test():
+def run_test(fileName):
     try:
+        subprocess.run(["cp", "medias/" + fileName, "../cypress/cypress/integration"])
         os.chdir('/srv/www/backend/cypress')
-        run_test = subprocess.run(["npx", "cypress", "run"], capture_output=True)
+        run_test = subprocess.run(["npx", "cypress", "run", "--spec", 'cypress/integration/' + fileName.split('/')[-1]], capture_output=True)
+        return run_test
     except Exception as e:
         raise e
 
