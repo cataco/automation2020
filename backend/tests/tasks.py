621b235d1bf786160fccf4eb0f094e69a4c5edb2
emantilla <e.mantilla@uniandes.edu.co>
2020-09-14 19:55:31 -0500

    add report to task

--- backend/tests/tasks.py
+++ backend/tests/tasks.py
@@ -7,10 +7,10 @@ import subprocess
 
 
 @app.task()
-def run_test():
+def run_test(fileName):
     try:
         os.chdir('/srv/www/backend/cypress')
-        run_test = subprocess.run(["npx", "cypress", "run"], capture_output=True)
+        run_test = subprocess.run(["npx", "cypress", "run", "--spec", fileName, "--reporter", "mochawesome"], capture_output=True)
     except Exception as e:
         raise e
 
