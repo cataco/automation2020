11b84aedececd0c82496548fe5d3dd11f2b23d31
jcardenasc93 <jcardenasc93@gmail.com>
2020-09-20 18:09:50 -0500

    Adds Random testing models to run monkey tests

--- backend/tests/admin.py
+++ backend/tests/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Framework, TestRequest, Reports
+from .models import Framework, TestRequest, Reports, RandomElement, RandomTest
 
 
 # Register your models here.
@@ -38,3 +38,27 @@ class ReportsAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Reports, ReportsAdmin)
+
+
+class RandomElementAdmin(admin.ModelAdmin):
+    list_display = (
+        'key',
+        'value'
+    )
+    list_filter = ['key']
+
+
+admin.site.register(RandomElement, RandomElementAdmin)
+
+
+class RandomTestAdmin(admin.ModelAdmin):
+    list_display = (
+        'createdAt',
+        'name',
+        'appUrl',
+        'eventsNumber',
+    )
+    list_filter = ['appUrl']
+
+
+admin.site.register(RandomTest, RandomTestAdmin)
--- backend/tests/models.py
+++ backend/tests/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+# Create your models here.
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -6,26 +8,43 @@ from django.dispatch import receiver
 class Framework(models.Model):
     name = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.name
-
 
 def setPath(instance, filename):
     return '{}/{}'.format(instance.framework.name, filename)
 
 
 class TestRequest(models.Model):
-    createdAt = models.DateTimeField(auto_now_add=True, verbose_name='creation_date')
+    createdAt = models.DateTimeField(
+        auto_now_add=True, verbose_name='creation_date')
     name = models.CharField(max_length=80)
-    framework = models.ForeignKey(Framework, on_delete=models.SET_NULL, null=True)
+    framework = models.ForeignKey(
+        Framework, on_delete=models.SET_NULL, null=True)
     testScript = models.FileField(upload_to=setPath)
 
+
+class RandomElement(models.Model):
+    key = models.CharField(max_length=20)
+    value = models.CharField(max_length=20)
+
     def __str__(self):
-        return str(self.name) + ': ' + self.testScript.name
+        return self.key + ':' + self.value
+
+
+class RandomTest(models.Model):
+    createdAt = models.DateTimeField(
+        auto_now_add=True, verbose_name='creation_date')
+    name = models.CharField(max_length=80)
+    appUrl = models.CharField(max_length=255)
+    eventsNumber = models.IntegerField()
+    randomElementsBehaivor = models.ManyToManyField(RandomElement)
+
+    def __str__(self):
+        return self.name + ' - ' + self.appUrl
 
 
 class Reports(models.Model):
-    createdAt = models.DateTimeField(auto_now_add=True, verbose_name='creation_date')
+    createdAt = models.DateTimeField(
+        auto_now_add=True, verbose_name='creation_date')
     test = models.ForeignKey(TestRequest, on_delete=models.CASCADE, null=True)
     testResults = models.TextField(max_length=1000)
 
@@ -33,4 +52,4 @@ class Reports(models.Model):
 @receiver(post_save, sender=TestRequest)
 def run_test_task(sender, instance, **kwargs):
     from tests.tasks import run_e2e_test
-    run_e2e_test.delay(instance.testScript.name, instance.id)
+    run_e2e_test.delay(instance.testScript.name, instance.pk)
