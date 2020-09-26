ee1f1325c075665574724cf32cb2b24053ec712b
jcardenasc93 <jcardenasc93@gmail.com>
2020-09-26 00:38:10 -0500

    Adds endpoints for all the tests

--- backend/tests/views.py
+++ backend/tests/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.generics import ListAPIView
-
-from tests.models import Reports
-from tests.serializers import ReportsSerializer
-
+from rest_framework.generics import ListAPIView, ListCreateAPIView
+from tests.models import Reports, TestStrategy, End2End, RandomTest, BDDTest
+from tests.serializers import ReportsSerializer, TestStrategySerializer, End2EndSerializer, RandomTestSerializer, BDDTestSerializer
 
 class ReportsViewSet(ListAPIView):
     serializer_class = ReportsSerializer
     queryset = Reports.objects.all()
+
+
+class TestStrategyView(ListCreateAPIView):
+    queryset = TestStrategy.objects.all()
+    serializer_class = TestStrategySerializer
+
+class End2EndView(ListCreateAPIView):
+    queryset = End2End.objects.all()
+    serializer_class = End2EndSerializer
+
+class RandomTestView(ListCreateAPIView):
+    queryset = RandomTest.objects.all()
+    serializer_class = RandomTestSerializer
+
+class BDDTestView(ListCreateAPIView):
+    queryset = BDDTest.objects.all()
+    serializer_class = BDDTestSerializer
