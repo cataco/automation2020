070feffe0d4add9f9fba02edbd3eb30fc06a82e4
SergioAndes <54184733+SergioAndes@users.noreply.github.com>
2020-10-24 09:21:21 -0500

    comit

--- frontend/aut-test-app/src/app/components/result-vrt/result-vrt.component.ts
+++ frontend/aut-test-app/src/app/components/result-vrt/result-vrt.component.ts
@@ -2,25 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import * as resemble from 'resemblejs';
 import { ResultService } from '../../services/result-service/result-service.service';
 
+
+class Resulta {
+  id: any;
+  imagen1: any;
+  imagen2: any;
+  resultado: any;
+  diferencia: any;
+  test: any;
+}
+
 @Component({
   selector: 'app-result-vrt',
   templateUrl: './result-vrt.component.html',
   styleUrls: ['./result-vrt.component.css']
 })
+
+
+
 export class ResultVrtComponent implements OnInit {
 
   diferencia;
   imagen1;
   imagen2;
   imagenDiff;
-  reports = [];
+  //reports = [];
+  public reports: Array<Resulta> = [];
+
+
+
   constructor(private resultService: ResultService) { }
 
   ngOnInit(): void {
     this.resultService.getVrtResult().subscribe(response => {
       console.log('results->', response);
       if (response){
-        this.reports = response;
+
+         response.forEach(dataItem => {
+           const res = new Resulta();
+           res.id = dataItem.id;
+           res.imagen1 = dataItem.image1;
+           res.imagen2 = dataItem.image2;
+           res.test = dataItem.test;
+           this.reports.push(res);
+         });
+       // this.reports = response;
       }
     });
   }
@@ -30,10 +56,8 @@ export class ResultVrtComponent implements OnInit {
     const report = this.getImages(id);
     const diff = resemble(report.imagen1)
       .compareTo(report.imagen2).onComplete(this.getDiff);
-    this.diferencia = sessionStorage.getItem('result');
-    this.imagenDiff = sessionStorage.getItem('imagen');
-
-
+    report.diferencia = sessionStorage.getItem('result');
+    report.resultado = sessionStorage.getItem('imagen');
   }
 
   // tslint:disable-next-line:typedef
